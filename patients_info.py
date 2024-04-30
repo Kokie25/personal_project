edbbe30f3c1f01c3dd6b809ca6037663d5595c8b
kmotshana023 <kmotshana023@student.wethinkcode.co.za>
2024-04-30 16:33:39 +0200

    updated

--- patients_info.py
+++ patients_info.py
@@ -6,6 +6,8 @@ from tkinter import messagebox
 from tkinter import ttk
 
 Data_file = "patients_records.json"
+employees_file = "employees_logins.json"
+admin_file = "admin.json"
 
 def save_data_to_json(data):
     """Save data to the JSON file."""
@@ -16,6 +18,27 @@ def load_data_to_json():
     if os.path.exists(Data_file):
         with open(Data_file, "r") as file:
             return json.load(file)
+        
+def save_employee_data_to_json(data):
+    """Save data to the JSON file."""
+    with open(employees_file, "w") as file:
+        json.dump(data, file, indent=4)
+
+def load_employee_data_to_json():
+    if os.path.exists(employees_file):
+        with open(employees_file, "r") as file:
+            return json.load(file)
+
+def save_admin_data_to_json(data):
+    """Save data to the JSON file."""
+    with open(admin_file, "w") as file:
+        json.dump(data, file, indent=4)
+
+def load_admin_data_to_json():
+    if os.path.exists(admin_file):
+        with open(admin_file, "r") as file:
+            return json.load(file)
+
 
 def unique_file_no():
     file_no = str(uuid4())
@@ -29,7 +52,7 @@ def validate_numbers_input(text):
         return False
     
 def validate_name_input(new_value):
-    return new_value.isalpha() or new_value == ""
+    return new_value.isalpha() or new_value == "" 
 
 def cancel_action():
 
@@ -50,6 +73,9 @@ def register():
         messagebox.showerror("Error", "Please enter a valid 10-digit contact numbers.")
         return
     alternative_contacts = Alternative_contacts.get()
+    if not alternative_contacts.isdigit() or len(str(alternative_contacts)) != 10:
+        messagebox.showerror("Error", "Please enter a valid 10-digit aternative numbers.")
+        return
     identityno = identity_entry.get()
     print("Got all inputs successfully...")
     
@@ -69,7 +95,8 @@ def register():
         "Initials": next_of_kin_entries[1].get(),
         "First Name": next_of_kin_entries[2].get(),
         "Last Name":next_of_kin_entries[3].get(),
-        "Contact details": next_of_kin_entries[4].get()
+        "Contact details": next_of_kin_entries[4].get()[:10]
+        
     }
     print("Created Physical Address and Next of Kin dicts...")
     
@@ -173,7 +200,8 @@ gender_combobox.pack(padx=10, pady=5, anchor="w")
 
 mobile_label = Label(scrollable_frame, text="Contact no:")
 mobile_label.pack(padx=10, pady=5, anchor="w")
-Mobile_entry = Entry(scrollable_frame, width=40)
+Mobile_entry = Entry(scrollable_frame, width=40,validate="key", validatecommand=(root.register(validate_numbers_input), "%P"),
+                             highlightcolor = 'green', bg='#F3FEFF',fg ="#393e46",highlightthickness = 1, bd=5,font='sans 10 bold')
 Mobile_entry.pack(padx=10, pady=5, anchor="w")
 
 altContact_label = Label(scrollable_frame, text="Alternative no:")
@@ -226,6 +254,9 @@ for label_text in next_of_kin_labels:
         kin_entry = ttk.Combobox(frame, values=title_options, state="readonly",width=34)
     else:
         kin_entry = Entry(frame, width=37,highlightcolor = 'green', bg='#F3FEFF',fg ="#393e46",highlightthickness = 1, bd=5,font='sans 10 bold')
+        if label_text == "Contact details:":
+            kin_entry = Entry(frame, width=37,validate="key", validatecommand=(root.register(validate_numbers_input), "%P"),
+                                highlightcolor = 'green', bg='#F3FEFF',fg ="#393e46",highlightthickness = 1, bd=5,font='sans 10 bold')
     
     kin_entry.pack(side="left", padx=(10, 0))
     next_of_kin_entries.append(kin_entry)
@@ -252,6 +283,177 @@ cancel_button.pack(side ="bottom",pady=10, anchor="w")
 register_button = Button(scrollable_frame, text="Register", command=register,fg="green",bg="white",highlightbackground = "lightgrey",highlightthickness = 1, bd=5,font='sans 10 bold')
 register_button.pack(side ="bottom",pady=10, anchor="w")
 
+def employees_logins():
 
+
+    system_username = username_entry.get().title()
+    system_password = password_entry.get()
+
+    new_employee = {
+        "Username": system_username,
+        "password": system_password,
+    }
+    try:
+        with open(employees_file, "r") as json_file:
+            data = json.load(json_file) 
+    except FileNotFoundError:
+        data = {"credentials": []}  
+
+    for employee in data["credentials"]:
+        if employee["Username"] == system_username and employee["password"] == system_password:
+            messagebox.showerror("Registration Failed", "Username and password combination already exists")
+            return  
+
+    data["credentials"].append(new_employee)
+    print("Appended new employee data...")
+
+    with open(employees_file, "w") as json_file:
+        json.dump(data, json_file, indent=4)
+    print("Data written to JSON file...")
+
+    messagebox.showinfo("Success", "Registration successful!")
+    print("Registration successful!")
+
+
+employee_register_window = Tk()
+employee_register_window.title("Register")
+
+
+username_label = Label(employee_register_window, text="Username:")
+username_label.pack()
+username_entry = Entry(employee_register_window)
+username_entry.pack()
+
+
+password_label = Label(employee_register_window, text="Password:")
+password_label.pack()
+password_entry = Entry(employee_register_window, show="*")  
+password_entry.pack()
+
+
+register_button = Button(employee_register_window, text="Register", command=employees_logins)
+register_button.pack()
+
+root.withdraw()
+
+def submit():
+
+    option = var.get()
+  
+    if option == 0:
+        messagebox.showinfo("Login", "Admin login selected")
+        check_admin_details()
+    elif option  == 1:
+        messagebox.showinfo("Login", "Employee login selected")
+        login()
+    else:
+        messagebox.showerror("Error", "Please select an option")
+
+submit_window = Tk()
+submit_window.title("Login")
+
+frame = Frame(submit_window)
+frame.pack(padx=20, pady=20)
+
+label = Label(frame, text="Select your role:")
+label.pack()
+
+var = IntVar(value=1)
+
+
+admin_radio = Radiobutton(frame, text="Admin", variable=var, value=0)
+admin_radio.pack(anchor=W)
+
+employee_radio = Radiobutton(frame, text="Employee", variable=var, value=1)
+employee_radio.pack(anchor=W)
+
+submit_button = Button(frame, text="Submit", command=submit)
+submit_button.pack()
+
+
+def check_admin_details():
+
+    data = load_admin_data_to_json()
+
+    admin_username = admin_username_entry.get
+    admin_password = admin_password_entry.get()
+
+    for admin in data["AdminAutho"]:
+        if admin_username == admin["username"] and admin_password == admin["password"]:
+            messagebox.showinfo("Admin Login", "Admin access granted.")
+            admin_window.destroy()  
+            employee_register_window.deiconify() 
+        else:
+            messagebox.showerror("Admin Login", "Admin access denied. Incorrect password or username,try again.")
+            admin_password_entry.delete(0, END)
+            admin_username_entry.delete(0,END) 
+    
+admin_window = Tk()
+admin_window.title("Admin Login")
+
+admin_username_label = Label(admin_window, text="Username:")
+admin_username_label.pack()
+admin_username_entry = Entry(admin_window)
+admin_username_entry.pack()
+
+admin_password_label = Label(admin_window, text="Admin Password:")
+admin_password_label.pack()
+admin_password_entry = Entry(admin_window, show="*")
+admin_password_entry.pack()
+
+
+login_button = Button(admin_window, text="Login", command=check_admin_details)
+login_button.pack()
+
+
+def login():
+
+    data = load_employee_data_to_json()
+    system_username = username_entry.get().title()
+    system_password = password_entry.get()
+    user_exists = False
+    password_correct = False
+    for employee in data["credentials"]:
+        if employee["Username"] == system_username:
+            user_exists = True    
+            if employee["Username"] == system_username and employee["password"] == system_password:
+                password_correct = True
+                messagebox.showinfo("Logged in",system_username + " Logged in successfully")
+                root.deiconify()  
+                login_window.withdraw()
+                return
+    
+    if user_exists == True and password_correct == False:
+        messagebox.showerror("Login Failed","Invalid username or password,try again or contact the admin")
+        return
+                
+    elif user_exists == False:
+        messagebox.showerror("Login Failed", "Login details do not exist,contact admin to register into the system as an employee")
+        
+
+login_window = Tk()
+login_window.title("Login")
+
+
+username_label = Label(login_window, text="Username:")
+username_label.pack()
+username_entry = Entry(login_window)
+username_entry.pack()
+
+
+password_label = Label(login_window, text="Password:")
+password_label.pack()
+password_entry = Entry(login_window, show="*")  
+password_entry.pack()
+
+
+login_button = Button(login_window, text="Login", command=login)
+login_button.pack()
+
+
+employee_register_window.withdraw()
+admin_window.withdraw()
+login_window.withdraw
+submit_window.mainloop()
 
 root.mainloop()
