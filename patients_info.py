d06389600dff7441d5e428cf572b476fd4b83651
kmotshana023 <kmotshana023@student.wethinkcode.co.za>
2024-05-10 16:49:05 +0200

    progress

--- patients_info.py
+++ patients_info.py
@@ -58,6 +58,168 @@ def cancel_action():
 
     if messagebox.askokcancel("Cancel", "Are you sure you want to cancel?"):
         root.destroy()
+def retrieve_info():
+
+    Identity = id_no_entry.get()
+    retrieve_data = load_data_to_json()
+    for retrieve in retrieve_data["patients"]:
+        if Identity == retrieve["IdentityNo"]:
+            First_name_entry.insert(0, retrieve["FullNames"])
+            Last_name_entry.insert(0, retrieve["LastName"])
+            title_combobox.set(retrieve["Title"])
+            identity_entry.insert(0, retrieve["IdentityNo"])
+            gender_combobox.set(retrieve["Gender"])
+            Mobile_entry.insert(0, retrieve["Contact_details"])
+            email_entry.insert(0, retrieve["Email_Address"])
+            Alternative_contacts.insert(0, retrieve["Alternative_contacts"])
+            for index, label_text in enumerate(physical_address_labels):
+                physical_address_entries[index].insert(0, retrieve["Physical Address"][label_text.split(":")[0]])
+            for index, label_text in enumerate(next_of_kin_labels):
+                if label_text != "Contact details:":
+                    next_of_kin_entries[index].insert(0, retrieve["Next_of_kin"][label_text.split(":")[0]])
+                else:
+                    next_of_kin_entries[index].insert(0, retrieve["Next_of_kin"]["Contact details"])
+            File_no_entry.insert(0, retrieve["FileNo"])
+            notes_combobox.set(retrieve["Notes"])
+            break
+
+retrieve_root = Tk()
+retrieve_root.title("Patient Registration Form")
+form_width = 500
+form_height =500
+retrieve_root.geometry(f"{form_width}x{form_height}")
+retrieve_root.configure(bg="lightblue")
+
+canvas = Canvas(retrieve_root)
+scrollbar = Scrollbar(retrieve_root, orient="vertical", command=canvas.yview)
+scrollable_frame = Frame(canvas)
+
+scrollable_frame.bind(
+    "<Configure>",
+    lambda e: canvas.configure(
+        scrollregion=canvas.bbox("all")
+    )
+)
+
+canvas.create_window((0, 0), window=scrollable_frame, anchor="nw")
+canvas.configure(yscrollcommand=scrollbar.set)
+
+canvas.pack(side="left", fill="both", expand=True)
+scrollbar.pack(side="right", fill="y")
+
+
+personal_label = Label(scrollable_frame, text="PERSONAL DETAILS", font=("Arial", 12, "bold"))
+personal_label.pack(padx=10, pady=10, anchor="w")
+
+first_name_label = Label(scrollable_frame, text="First Name:")
+validate_name = retrieve_root.register(validate_name_input)
+first_name_label.pack(padx=10, pady=5, anchor="w")
+First_name_entry = Entry(scrollable_frame, width=40,validate="key", validatecommand =(validate_name, "%P"),
+                         highlightcolor = 'green', bg='#F3FEFF',fg ="#393e46",highlightthickness = 1, bd=5,font='sans 10 bold')
+First_name_entry.pack(padx=10, pady=5, anchor="w")
+
+last_name_label = Label(scrollable_frame, text="Last Name:")
+last_name_label.pack(padx=10, pady=5, anchor="w")
+validate_lastname = retrieve_root.register(validate_name_input)
+Last_name_entry = Entry(scrollable_frame, width=40,validate="key", validatecommand =(validate_lastname, "%P"),highlightcolor = 'green', bg='#F3FEFF',fg ="#393e46"
+                    ,highlightthickness = 1, bd=5,font='sans 10 bold')
+Last_name_entry.pack(padx=10, pady=5, anchor="w")
+
+
+title_label = ttk.Label(scrollable_frame, text="Title:")
+title_label.pack(padx=10, pady=5, anchor="w")
+title_combobox = ttk.Combobox(scrollable_frame, values=["Mr", "Ms", "Mrs", "Miss", "Dr"],state="readonly", width=37)
+title_combobox.pack(padx=10, pady=5, anchor="w")
+
+identity_label = Label(scrollable_frame, text="SA ID number:")
+identity_label.pack(padx=10, pady=5, anchor="w")
+identity_entry = Entry(scrollable_frame, width=40, validate="key", validatecommand=(retrieve_root.register(validate_numbers_input), "%P"),
+                       highlightcolor = 'green', bg='#F3FEFF',fg ="#393e46",highlightthickness = 1, bd=5,font='sans 10 bold')
+identity_entry.pack(padx=10, pady=5, anchor="w")
+
+gender_label = Label(scrollable_frame, text="Gender:")
+gender_label.pack(padx=10, pady=5, anchor="w")
+gender_combobox = ttk.Combobox(scrollable_frame, values=["Male", "Female", "Other"],state="readonly", width=37)
+gender_combobox.pack(padx=10, pady=5, anchor="w")
+
+mobile_label = Label(scrollable_frame, text="Contact no:")
+mobile_label.pack(padx=10, pady=5, anchor="w")
+Mobile_entry = Entry(scrollable_frame, width=40,validate="key", validatecommand=(retrieve_root.register(validate_numbers_input), "%P"),
+                             highlightcolor = 'green', bg='#F3FEFF',fg ="#393e46",highlightthickness = 1, bd=5,font='sans 10 bold')
+Mobile_entry.pack(padx=10, pady=5, anchor="w")
+
+altContact_label = Label(scrollable_frame, text="Alternative no:")
+altContact_label.pack(padx=10, pady=5, anchor="w")
+Alternative_contacts = Entry(scrollable_frame, width=40,validate="key", validatecommand=(retrieve_root.register(validate_numbers_input), "%P"),
+                             highlightcolor = 'green', bg='#F3FEFF',fg ="#393e46",highlightthickness = 1, bd=5,font='sans 10 bold')
+Alternative_contacts.pack(padx=10, pady=5, anchor="w")
+
+email_label = Label(scrollable_frame, text="Email Address:")
+email_label.pack(padx=10, pady=5, anchor="w")
+email_entry = Entry(scrollable_frame, width=40,highlightcolor = 'green', bg='#F3FEFF',fg ="#393e46",highlightthickness = 1, bd=5,font='sans 10 bold')
+email_entry.pack(padx=10, pady=5, anchor="w")
+
+phys_address_label = Label(scrollable_frame, text="PHYSICAL ADDRESS", font=("Arial", 12, "bold"))
+phys_address_label.pack(padx=10, pady=10, anchor="w")
+
+physical_address_labels = [
+    "Street address:", "Surburb:", "City:", "Zip code:"
+]
+physical_address_entries = []
+
+for label_text in physical_address_labels:
+    frame = Frame(scrollable_frame)
+    frame.pack(padx=10, pady=5, anchor="w")
+
+    label = Label(frame, text=label_text)
+    label.pack(side="left")
+
+    address_entry = Entry(frame, width=37,highlightcolor = 'green', bg='#F3FEFF',fg ="#393e46",highlightthickness = 1, bd=5,font='sans 10 bold')
+    address_entry.pack(side="left", padx=(10, 0))
+    physical_address_entries.append(address_entry)
+
+next_of_kin_label = Label(scrollable_frame, text="NEXT OF KIN DETAILS", font=("Arial", 12, "bold"))
+next_of_kin_label.pack(padx=10, pady=10, anchor="w")
+
+next_of_kin_labels = [
+    "Title:", "Initials:", "First Name:","Last Name:","Contact details:"
+]
+next_of_kin_entries = []
+
+for label_text in next_of_kin_labels:
+    frame = Frame(scrollable_frame)
+    frame.pack(padx=10, pady=5, anchor="w")
+
+    label = Label(frame, text=label_text)
+    label.pack(side="left")
+
+    if label_text == "Title:":
+        title_options = ["Mr", "Ms", "Mrs", "Miss", "Dr"]
+        kin_entry = ttk.Combobox(frame, values=title_options, state="readonly",width=34)
+    else:
+        kin_entry = Entry(frame, width=37,highlightcolor = 'green', bg='#F3FEFF',fg ="#393e46",highlightthickness = 1, bd=5,font='sans 10 bold')
+        if label_text == "Contact details:":
+            kin_entry = Entry(frame, width=37,validate="key", validatecommand=(retrieve_root.register(validate_numbers_input), "%P"),
+                                highlightcolor = 'green', bg='#F3FEFF',fg ="#393e46",highlightthickness = 1, bd=5,font='sans 10 bold')
+    
+    kin_entry.pack(side="left", padx=(10, 0))
+    next_of_kin_entries.append(kin_entry)
+
+
+file_no = str(uuid4())[:8]  
+file_label = Label(scrollable_frame, text="File No:")
+file_label.pack(padx=10, pady=5, anchor="w")
+File_no_entry = Entry(scrollable_frame, width=40,highlightcolor = 'green', bg='#F3FEFF', fg='black', highlightbackground='green',highlightthickness = 1, bd=5,font='sans 10 bold')
+
+File_no_entry.insert(0, file_no)
+File_no_entry.pack(padx=10, pady=5, anchor="w")
+
+notes_label = Label(scrollable_frame, text="Description:")
+notes_label.pack(padx=10, pady=5, anchor="w")
+notes_combobox = ttk.Combobox(scrollable_frame, values=["Consultation", "Immunisation", "Collecting Medication","Checkup","Vaccination"],state="readonly",width=37)
+notes_combobox.pack(padx=10, pady=5, anchor="w")
+
+retrieve_root.withdraw()
 
 def register():
     
@@ -283,7 +445,18 @@ cancel_button.pack(side ="bottom",pady=10, anchor="w")
 register_button = Button(scrollable_frame, text="Register", command=register,fg="green",bg="white",highlightbackground = "lightgrey",highlightthickness = 1, bd=5,font='sans 10 bold')
 register_button.pack(side ="bottom",pady=10, anchor="w")
 
-def employees_logins():
+retrieve_data = Button(scrollable_frame, text="Retrieve Data", command=retrieve_info,fg="green",bg="white",highlightbackground = "lightgrey",highlightthickness = 1, bd=5,font='sans 10 bold')
+register_button.pack(side ="bottom",pady=10, anchor="w")
+
+def is_admin_clerk(system_username):
+
+    admin_data = load_admin_data_to_json()
+    for admin in admin_data[ "AdminAutho"]:
+        if admin["username"] == system_username:
+            return True
+    return False
+
+def employees_registration():
 
 
     system_username = username_entry.get().title()
@@ -293,6 +466,7 @@ def employees_logins():
         "Username": system_username,
         "password": system_password,
     }
+    print(new_employee)
     try:
         with open(employees_file, "r") as json_file:
             data = json.load(json_file) 
@@ -331,104 +505,47 @@ password_entry = Entry(employee_register_window, show="*")
 password_entry.pack()
 
 
-register_button = Button(employee_register_window, text="Register", command=employees_logins)
+register_button = Button(employee_register_window, text="Register", command= employees_registration)
 register_button.pack()
 
-root.withdraw()
-
-def submit():
-
-    option = var.get()
-  
-    if option == 0:
-        messagebox.showinfo("Login", "Admin login selected")
-        check_admin_details()
-    elif option  == 1:
-        messagebox.showinfo("Login", "Employee login selected")
-        login()
-    else:
-        messagebox.showerror("Error", "Please select an option")
-
-submit_window = Tk()
-submit_window.title("Login")
-
-frame = Frame(submit_window)
-frame.pack(padx=20, pady=20)
-
-label = Label(frame, text="Select your role:")
-label.pack()
-
-var = IntVar(value=1)
+employee_register_window.withdraw()
 
 
-admin_radio = Radiobutton(frame, text="Admin", variable=var, value=0)
-admin_radio.pack(anchor=W)
-
-employee_radio = Radiobutton(frame, text="Employee", variable=var, value=1)
-employee_radio.pack(anchor=W)
-
-submit_button = Button(frame, text="Submit", command=submit)
-submit_button.pack()
-
-
-def check_admin_details():
-
-    data = load_admin_data_to_json()
-
-    admin_username = admin_username_entry.get
-    admin_password = admin_password_entry.get()
-
-    for admin in data["AdminAutho"]:
-        if admin_username == admin["username"] and admin_password == admin["password"]:
-            messagebox.showinfo("Admin Login", "Admin access granted.")
-            admin_window.destroy()  
-            employee_register_window.deiconify() 
-        else:
-            messagebox.showerror("Admin Login", "Admin access denied. Incorrect password or username,try again.")
-            admin_password_entry.delete(0, END)
-            admin_username_entry.delete(0,END) 
-    
-admin_window = Tk()
-admin_window.title("Admin Login")
-
-admin_username_label = Label(admin_window, text="Username:")
-admin_username_label.pack()
-admin_username_entry = Entry(admin_window)
-admin_username_entry.pack()
-
-admin_password_label = Label(admin_window, text="Admin Password:")
-admin_password_label.pack()
-admin_password_entry = Entry(admin_window, show="*")
-admin_password_entry.pack()
-
-
-login_button = Button(admin_window, text="Login", command=check_admin_details)
-login_button.pack()
 
 
 def login():
 
     data = load_employee_data_to_json()
-    system_username = username_entry.get().title()
-    system_password = password_entry.get()
+    data_admin = load_admin_data_to_json()
+    system_username = login_username_entry.get().title()
+    system_password = login_password_entry.get()
     user_exists = False
     password_correct = False
-    for employee in data["credentials"]:
+
+    for employee in data.get("credentials"):
         if employee["Username"] == system_username:
             user_exists = True    
             if employee["Username"] == system_username and employee["password"] == system_password:
                 password_correct = True
-                messagebox.showinfo("Logged in",system_username + " Logged in successfully")
-                root.deiconify()  
+                messagebox.showinfo("Logged in", system_username + " Logged in successfully")
+                root.deiconify()
                 login_window.withdraw()
                 return
-    
-    if user_exists == True and password_correct == False:
-        messagebox.showerror("Login Failed","Invalid username or password,try again or contact the admin")
-        return
-                
-    elif user_exists == False:
-        messagebox.showerror("Login Failed", "Login details do not exist,contact admin to register into the system as an employee")
+
+    for admin in data_admin.get("AdminAutho"):
+        if admin["admin_username"] == system_username:
+            user_exists = True
+            if admin["admin_username"] == system_username and admin["admin_password"] == system_password:
+                password_correct = True
+                employee_register_window.deiconify()
+                login_window.withdraw()
+                return
+
+    if user_exists and not password_correct:
+        messagebox.showerror("Login Failed", "Invalid username or password, try again or contact the admin")
+    elif not user_exists:
+        messagebox.showerror("Login Failed", "Login details do not exist, contact admin to register into the system as an employee")
+
         
 
 login_window = Tk()
@@ -437,23 +554,19 @@ login_window.title("Login")
 
 username_label = Label(login_window, text="Username:")
 username_label.pack()
-username_entry = Entry(login_window)
-username_entry.pack()
+login_username_entry = Entry(login_window)
+login_username_entry.pack()
 
 
 password_label = Label(login_window, text="Password:")
 password_label.pack()
-password_entry = Entry(login_window, show="*")  
-password_entry.pack()
+login_password_entry = Entry(login_window, show="*")  
+login_password_entry.pack()
 
 
 login_button = Button(login_window, text="Login", command=login)
 login_button.pack()
 
 
-employee_register_window.withdraw()
-admin_window.withdraw()
-login_window.withdraw
-submit_window.mainloop()
-
-root.mainloop()
+root.withdraw()
+login_window.mainloop()
